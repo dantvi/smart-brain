7cba928b226982195fdea1ed034cb786433e2af4
dantvi <daniel.tving@gmail.com>
2024-10-12 13:56:39 +0200

    particles background

---
+++ src/components/rank/rank.component.jsx
@@ -0,0 +1,15 @@
+
+const Rank = () => {
+  return (
+    <div>
+      <div className="white f3">
+        {`Daniel, your current entry count is...`}
+      </div>
+      <div className="white f1">
+        {`#3`}
+      </div>
+    </div>
+  );
+}
+
+export default Rank;
