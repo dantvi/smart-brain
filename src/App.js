7d0b661254ad5f30b4990329e236a198ca86d892
Daniel Tving <daniel.tving@gmail.com>
2025-06-19 13:54:05 +0200

    include JWT token in image requests from frontend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import { Component } from 'react';
 import ParticlesBg from 'particles-bg';
-import Navigation from "./components/navigation/navigation.component";
+import Navigation from './components/navigation/navigation.component';
 import SignIn from './components/sign-in/sign-in.component';
 import Register from './components/register/register.component';
-import Logo from "./components/logo/logo.component";
-import Rank from "./components/rank/rank.component";
-import ImageLinkForm from "./components/image-link-form/image-link-form.component";
+import Logo from './components/logo/logo.component';
+import Rank from './components/rank/rank.component';
+import ImageLinkForm from './components/image-link-form/image-link-form.component';
 import FaceRecognition from './components/face-recognition/face-recognition.component';
 import './App.css';
-
 
 const initialState = {
   input: '',
@@ -21,9 +20,9 @@ const initialState = {
     name: '',
     email: '',
     entries: 0,
-    joined: ''
-  }
-}
+    joined: '',
+  },
+};
 
 class App extends Component {
   constructor() {
@@ -38,61 +37,71 @@ class App extends Component {
         name: data.name,
         email: data.email,
         entries: data.entries,
-        joined: data.joined
-      }
+        joined: data.joined,
+      },
     });
-  }
+  };
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
-    }
-  }
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
 
   displayFaceBox = (box) => {
     this.setState({ box: box });
-  }
+  };
 
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
-  }
+  };
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
+
+    const token = localStorage.getItem('token');
+
     fetch('http://localhost:3000/imageurl', {
       method: 'post',
-      headers: { 'Content-Type': 'application/json' },
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token,
+      },
       body: JSON.stringify({
-        input: this.state.input
-      })
+        input: this.state.input,
+      }),
     })
-      .then(response => response.json())
-      .then(response => {
+      .then((response) => response.json())
+      .then((response) => {
         if (response) {
           fetch('http://localhost:3000/image', {
             method: 'put',
-            headers: { 'Content-Type': 'application/json' },
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: 'Bearer ' + token,
+            },
             body: JSON.stringify({
-              id: this.state.user.id
-            })
+              id: this.state.user.id,
+            }),
           })
-            .then(response => response.json())
-            .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count }))
+            .then((response) => response.json())
+            .then((count) => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
             })
-            .catch(console.log)
+            .catch(console.log);
         }
-        this.displayFaceBox(this.calculateFaceLocation(response))
+        this.displayFaceBox(this.calculateFaceLocation(response));
       })
-      .catch(err => console.log(err));
-  }
+      .catch((err) => console.log(err));
+  };
 
   onRouteChange = (route) => {
     if (route === 'sign-out') {
@@ -101,28 +110,38 @@ class App extends Component {
       this.setState({ isSignedIn: true });
     }
     this.setState({ route: route });
-  }
+  };
 
   render() {
     const { isSignedIn, imageUrl, route, box } = this.state;
     return (
-      <div className="App">
-        <ParticlesBg num={5} type="circle" bg={true} />
-        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
-        {route === 'home' ?
+      <div className='App'>
+        <ParticlesBg num={5} type='circle' bg={true} />
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
+        {route === 'home' ? (
           <div>
             <Logo />
-            <Rank name={this.state.user.name} entries={this.state.user.entries} />
-            <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
             <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
-          : (
-            route === 'sign-in' || route === 'sign-out' ?
-              <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
-              :
-              <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
-          )
-        }
+        ) : route === 'sign-in' || route === 'sign-out' ? (
+          <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
+        )}
       </div>
     );
   }
